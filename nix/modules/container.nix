7d7760261851dad15f56071401393076b0e5b275
LilDojd <yawner@pm.me>
2026-03-24 16:27:38 +0400

    fix(docker): try to fix docker build
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: Ic4e5e468eec391351c442de5dd1f972f6a6a6964

--- nix/modules/container.nix
+++ nix/modules/container.nix
@@ -4,29 +4,49 @@
     let
       nix2container = inputs.nix2container.packages.${system}.nix2container;
 
+      crossPkgs = pkgs.pkgsCross.aarch64-multiplatform;
+
       containerTargets = lib.optionalAttrs (system == "x86_64-linux") {
         amd64 = {
           arch = "amd64";
           package = self'.packages.pagers;
+          rootPkgs = pkgs;
         };
         arm64 = {
           arch = "arm64";
           package = self'.packages.pagers-aarch64-unknown-linux-gnu;
+          rootPkgs = crossPkgs;
         };
       };
 
-      makeContainerImage = name: { arch, package }:
+      makeContainerImage = name: { arch, package, rootPkgs }:
+        let
+          rootEnv = rootPkgs.buildEnv {
+            name = "root";
+            paths = [ package rootPkgs.bashInteractive rootPkgs.coreutils ];
+            pathsToLink = [ "/bin" ];
+          };
+        in
         nix2container.buildImage {
           name = "pagers";
           tag = "latest-${arch}";
           inherit arch;
 
-          copyToRoot = [ package ];
+          copyToRoot = [ rootEnv ];
+
+          layers = [
+            (nix2container.buildLayer {
+              deps = [ rootPkgs.bashInteractive rootPkgs.coreutils ];
+              maxLayers = 80;
+            })
+          ];
+
           config = {
-            entrypoint = [ "${package}/bin/pagers" ];
+            entrypoint = [ "/bin/pagers" ];
+            Cmd = [ "--help" ];
           };
 
-          maxLayers = 50;
+          maxLayers = 10;
         };
 
       containers = lib.mapAttrs'
