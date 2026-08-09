4f340967b1a032d92342c6c0ed0a49e30354a9c9
LilDojd <yawner@pm.me>
2026-08-09 21:57:01 +0400

    chore: package hawk to devShell
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: I98aecdcaf5e5891d5e150151b2b04b3c6a6a6964

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -26,6 +26,44 @@
               };
             }
         else craneLib.devShell;
+
+      hawkRelease =
+        {
+          x86_64-linux = {
+            target = "x86_64-unknown-linux-gnu";
+            hash = "sha256-+cr7vFd3srAMNiYZuLYq+ULSkAoSGsri5zIQqOv/2Fc=";
+          };
+          aarch64-linux = {
+            target = "aarch64-unknown-linux-gnu";
+            hash = "sha256-iWo5o5junMD8tRNUx48rLEe06/Eo5w9RmeKQVwff0f8=";
+          };
+        }.${system};
+
+      hawkToolchain =
+        (inputs.fenix.packages.${system}.toolchainOf {
+          channel = "1.97.1";
+          sha256 = "sha256-A1abGIbOtcBSdrUMhDGrER3pRM1hQP4fp9gh3Y4PKc8=";
+        }).minimalToolchain;
+
+      cargo-hawk = pkgs.stdenv.mkDerivation {
+        pname = "cargo-hawk";
+        version = "0.1.12";
+        src = pkgs.fetchurl {
+          url = "https://github.com/astral-sh/hawk/releases/download/0.1.12/cargo-hawk-${hawkRelease.target}.tar.gz";
+          inherit (hawkRelease) hash;
+        };
+
+        nativeBuildInputs = [ pkgs.autoPatchelfHook pkgs.makeWrapper ];
+        buildInputs = [ pkgs.stdenv.cc.cc.lib ];
+        autoPatchelfIgnoreMissingDeps = [ "librustc_driver-*.so" ];
+
+        installPhase = ''
+          runHook preInstall
+          install -Dm755 cargo-hawk cargo-hawk-driver -t $out/bin
+          wrapProgram $out/bin/cargo-hawk --prefix PATH : ${lib.makeBinPath [ hawkToolchain ]}
+          runHook postInstall
+        '';
+      };
     in
     {
       devShells.default = devShell {
@@ -55,6 +93,7 @@
             omnix
           ]
           ++ lib.optionals pkgs.stdenv.isLinux [
+            cargo-hawk
             perf
           ];
       };
